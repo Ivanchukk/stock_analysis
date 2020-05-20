f4911cdca63bac2df43bfe0457ff1608221a98b3
ivan lisi <ivanlisi88@gmail.com>
2020-05-20 20:48:26 +0300

    Working on big data

--- dataBase/dataQuarry.py
+++ dataBase/dataQuarry.py
@@ -8,15 +8,15 @@ class StockDataBase:
         self.cur = self.conn.cursor()
 
     def add_data(self,company_quotes_df):
-        company_quotes_df.to_sql(name='stocks',if_exists='append', con=self.conn)
-        self.cur.execute("SELECT * FROM stocks")
+        company_quotes_df.to_sql(name='stocks_full',if_exists='append', con=self.conn)
+        self.cur.execute("SELECT * FROM stocks_full")
         g = self.cur.fetchall()
         print(g)
         return g
 
     def retrieve_date(self, a):
-        self.cur.execute("SELECT date, symbol, price, changesPercentage, change, volume, avgVolume  FROM stocks WHERE date in ", (a,))
+        a = tuple(a)
+        self.cur.execute("SELECT date, symbol, price, changesPercentage, change, volume, avgVolume  FROM stocks WHERE date in {}".format(a))
         g = self.cur.fetchall()
-
         return g
 
