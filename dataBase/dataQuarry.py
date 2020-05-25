dc1d5cdc8f9fc566d8922c971e170aba4f047641
ivan lisi <ivanlisi88@gmail.com>
2020-05-25 19:41:22 +0300

    Adding 3600 stocks with daily data

--- dataBase/dataQuarry.py
+++ dataBase/dataQuarry.py
@@ -8,15 +8,20 @@ class StockDataBase:
         self.cur = self.conn.cursor()
 
     def add_data(self,company_quotes_df):
-        company_quotes_df.to_sql(name='stocks_full',if_exists='append', con=self.conn)
-        self.cur.execute("SELECT * FROM stocks_full")
+        company_quotes_df.to_sql(name='stocks',if_exists='append', con=self.conn)
+        self.cur.execute("SELECT * FROM stocks")
         g = self.cur.fetchall()
         print(g)
         return g
 
     def retrieve_date(self, a):
+        self.cur.execute("SELECT date, symbol, price,priceAvg50,priceAvg200, changesPercentage, change, volume, avgVolume, pe, eps FROM stocks")
+        g = self.cur.fetchall()
+        print(g)
+        return g
+
         a = tuple(a)
-        self.cur.execute("SELECT date, symbol, price, changesPercentage, change, volume, avgVolume  FROM stocks WHERE date in {}".format(a))
+        self.cur.execute("SELECT date, symbol, price,priceAvg50,priceAvg200, changesPercentage, change, volume, avgVolume, pe, eps  FROM stocks WHERE date in {}".format(a))
         g = self.cur.fetchall()
         return g
 
--- datascraping/tests.py
+++ datascraping/tests.py
@@ -1,7 +1,40 @@
-import requests
+import numpy as np
 import pandas as pd
+import requests
+from datetime import date
+from dataBase.dataQuarry import *
 
-histprices = requests.get("https://fmpcloud.io/api/v3/historical-price-full/GOOG?serietype=line&apikey=GOOG")
-histprices = histprices.json()
+class Stocks:
+    def __init__(self):
 
-print(histprices)
+# Creating DataFrame with all usa stocks and filter in to stocks in range of 0.2 < stocks < 10
+        self.all_us_stocks = requests.get('https://financialmodelingprep.com/api/v3/company/stock/list?apikey=2c50c486fd2834b02f93e296db5d3742')
+        self.all_us_stocks = self.all_us_stocks.json()
+        self.a = pd.DataFrame(self.all_us_stocks)
+
+        self.all_us_stocks_df = pd.DataFrame.from_dict(self.all_us_stocks['symbolsList'])
+
+# From now in all_us_stocks_df will hold stpcks in the range 10 > all_us_stocks_df > 0.2
+        self.all_us_stocks_df = self.all_us_stocks_df[self.all_us_stocks_df.price > 0.2]
+        self.all_us_stocks_df = self.all_us_stocks_df[self.all_us_stocks_df.price < 10]
+        self.all_us_stocks_df = self.all_us_stocks_df.sort_values(by='price', ascending=True)
+
+# Create list out of symbol column
+        self.stock_lst = self.all_us_stocks_df['symbol'].tolist()
+        self.stock_lst_str = "'"+','.join(self.stock_lst)+"'"
+
+
+    def cheap_stock_df(self):
+        print(len(self.stock_lst))
+        nn = self.stock_lst
+        h = len(nn)/500
+        print(h)
+        print(int(h))
+        m = len(self.stock_lst) / 7
+        print(m)
+        mm = len(self.stock_lst) - int(h)*500
+        print(mm)
+
+
+aa = Stocks()
+aa.cheap_stock_df()
